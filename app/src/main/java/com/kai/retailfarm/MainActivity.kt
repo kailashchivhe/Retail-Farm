e3a3222046cb7bba4c3e70cc9cfc82061fcf5e4d
Kailash Chivhe <kchivhe@gmail.com>
2020-04-27 21:01:46 +0530

    Initial Splash screen support and app icons
    - Added Splash Screen with app icon.
    - Added icon on action bar.
    - Changed launcher activity to SplashScreen.

--- app/src/main/java/com/kai/retailfarm/MainActivity.kt
+++ app/src/main/java/com/kai/retailfarm/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setHomeAsUpIndicator(  R.drawable.action_bar_icon )
+        supportActionBar?.setDisplayHomeAsUpEnabled( true )
     }
 }
---
+++ app/src/main/java/com/kai/retailfarm/SplashActiviy.kt
@@ -0,0 +1,34 @@
+package com.kai.retailfarm
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rbddevs.splashy.Splashy
+
+class SplashActiviy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Splashy(this)
+            .setLogo(R.drawable.app_icon_hd)
+            .setTitle( "RETAIL FARM" )
+            .setTitleColor( R.color.white )
+            .setBackgroundResource(R.color.colorPrimary)
+            .setFullScreen(true)
+            .setTime(2000)
+            .show()
+
+        Splashy.onComplete( object : Splashy.OnComplete {
+            override fun onComplete() {
+                startMainActivity()
+            }
+        })
+    }
+
+    fun startMainActivity()
+    {
+        val intent = Intent( this, MainActivity::class.java)
+        finishAffinity()
+        startActivity(intent)
+    }
+}
