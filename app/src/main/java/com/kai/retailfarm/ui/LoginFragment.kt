ada044ff5795ea8c798bbe873df28fe36b3aa9dc
Kailash Chivhe <kchivhe@gmail.com>
2020-04-27 23:16:01 +0530

    Initial Firebase support and package structure changed

--- app/src/main/java/com/kai/retailfarm/ui/LoginFragment.kt
+++ app/src/main/java/com/kai/retailfarm/ui/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.kai.retailfarm
+package com.kai.retailfarm.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kai.retailfarm.R
 
 /**
  * A simple [Fragment] subclass.
